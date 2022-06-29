a9358abe5335af4d1b643f1e7d41a7c182938a34
M4rrt <guilhermemartinszot@gmail.com>
2022-06-29 11:32:11 -0300

    pagina 22

--- rrproject/src/App.js
+++ rrproject/src/App.js
@@ -9,30 +9,50 @@ const welcome = {
 const list = [
   {
     title: 'Facebook',
-    url: 'facebook.com.br',
+    url: 'https://facebook.com',
     autor: 'meta',
     num_coments: '1000',
     objectId: '0',
   },
   {
     title: 'youtube',
-    url: 'youtube.com.br',
+    url: 'https://youtube.com',
     autor: 'google',
     num_coments: '200',
     objectId: '1',
   }
 ]
 
+const numbers = [1,4,8,10]
+
+const newNumbers= numbers.map(function(number){
+  return number * 2
+})
+
 function App() {
   const titulo = "M4rtt";
   return (
     <div className="App">
-      <img src={logo} alt="" />
+      <img src={logo} alt={titulo} />
       <h1>{welcome.greeting} <b>{welcome.name}</b></h1>
       
       
       <label htmlFor="search"> Search </label>
       <input id="search" type="text"></input>
+
+      {list.map(function(item){
+        return (
+        <div id={item.objectId}>
+          <h1>
+          {item.title}
+          </h1>
+          <span>
+            <a target="_blank" href={item.url} rel="noreferrer">Site</a>
+          </span><br/>
+          <span>{item.autor}</span> <br/>
+          <span>{item.num_coments}</span>
+        </div>
+        );})}
     </div>
   );
 }
